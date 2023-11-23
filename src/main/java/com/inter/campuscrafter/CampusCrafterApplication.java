ebe9452d03eec162b229dae7ee19603a76ca2988
artyommargaryan <artyom.margaryan.02@gmail.com>
2023-11-23 16:18:12 +0400

    adding main

---
+++ src/main/java/com/inter/campuscrafter/CampusCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.inter.campuscrafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCrafterApplication.class, args);
+	}
+
+}
